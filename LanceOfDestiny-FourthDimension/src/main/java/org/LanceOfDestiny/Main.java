e46f90602f6034357a6a51bed61a09f550c61d8b
TunaCimen <tcimen21@ku.edu.tr>
2024-03-05 18:36:14 +0300

    add maindffdf

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/Main.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/Main.java
@@ -2,7 +2,7 @@ package org.LanceOfDestiny;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloodssdsrld!");
+        System.out.println("Helloodsssdsddsrld!");
 
     }
 }
