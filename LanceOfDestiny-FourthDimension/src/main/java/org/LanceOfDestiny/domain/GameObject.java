98c0390086edb1424d2be369bd8bd62d9de8ff66
Ege Erdem Özlü <eozlu21@ku.edu.tr>
2024-04-10 20:53:52 +0300

    Addition of components to GameObject

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/GameObject.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/GameObject.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public abstract class GameObject {
 
-    private static ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
+    private static List<GameObject> gameObjects = new ArrayList<>();
+    private LinkedList<Object> components = new LinkedList<>();
 
     public GameObject() {
         GameObject.gameObjects.add(this);
@@ -18,16 +19,39 @@ public abstract class GameObject {
     public void Update() {
     }
 
-    public static ArrayList<GameObject> getGameObjects() {
+    public static List<GameObject> getGameObjects() {
         return gameObjects;
     }
 
     public void Destroy() {
         gameObjects.remove(this);
+        var components = getComponents();
+        // this should remove all components associated with the game object hopefully
+        for (int i = 0; i < components.size(); i++) {
+            components.removeFirst();
+        }
     }
 
     public static void DestroyAll(){
         gameObjects.clear();
     }
+
+    public LinkedList<Object> getComponents() {
+        return components;
+    }
+
+    public void setComponents(LinkedList<Object> components) {
+        this.components = components;
+    }
+
+    public <T> T getComponent(Class<T> componentType) {
+        for (Object component : components) {
+            if (componentType.isInstance(component)) {
+                return componentType.cast(component);
+            }
+        }
+        return null; // or optionally throw an exception if the component is not found
+    }
+    // Barrier barrier = getComponent(Barrier.class); an example usage
 
 }
