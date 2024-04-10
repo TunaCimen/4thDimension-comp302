b1e6f9dfee5cf7309c319821d7294ab4d8b853cc
idilissever <idilissever@gmail.com>
2024-04-10 23:19:09 +0300

    implementation of fireball and magicalStaff

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/player/MagicalStaff.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/player/MagicalStaff.java
@@ -1,6 +1,24 @@
 package org.LanceOfDestiny.domain.player;
 
+import org.LanceOfDestiny.domain.Constants;
 import org.LanceOfDestiny.domain.GameObject;
 
 public class MagicalStaff extends GameObject {
+
+    private int width = Constants.STAFF_WIDTH;
+    private int height = Constants.STAFF_HEIGHT;
+
+    private double x;
+    private double y;
+
+    private boolean isCanonActivated = false;
+    private boolean isExpanded = false;
+
+    public MagicalStaff() {
+        super();
+
+    }
+
+
+
 }
