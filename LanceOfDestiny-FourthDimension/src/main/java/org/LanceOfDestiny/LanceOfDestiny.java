101b3935fa51eaedd6b4036c16db6fbf120088e4
TunaCimen <tcimen21@ku.edu.tr>
2024-04-10 14:27:20 +0300

    EventManager

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/LanceOfDestiny.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/LanceOfDestiny.java
@@ -8,16 +8,7 @@ public class LanceOfDestiny {
     public static void main(String[] args) throws SQLException {
         WindowManager wm = WindowManager.getInstance();
         wm.showWindow(Windows.Login);
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://127.0.0.1:3306/Users302",
-                "root",
-                "Whitewolf2206"
-        );
-        Statement statement = connection.createStatement();
-        ResultSet resultset = statement.executeQuery("SELECT * FROM USERS");
-        while(resultset.next()) {
-            System.out.println(resultset.getString("username"));
-        }
+
 
     }
 }
