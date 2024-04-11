5c1800999b91eaab5e315b1916fb26d34c91f173
TunaCimen <100401642+TunaCimen@users.noreply.github.com>
2024-04-11 13:54:53 +0300

    changes

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/Behaviour.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/Behaviour.java
@@ -1,5 +1,6 @@
 package org.LanceOfDestiny.domain;
 
+<<<<<<< Updated upstream
 public abstract class Behaviour {
 
     public void Awake() {
@@ -9,6 +10,33 @@ public abstract class Behaviour {
     }
 
     public void Update() {
+=======
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Behaviour {
+
+    public Transform transform;
+    ArrayList<Object> components;
+
+    public Behaviour(){
+        components = new ArrayList<>(Collections.singletonList(transform));
+        transform = new Transform(0,0,0);
+    }
+
+    public Object getComponent(Object component){
+        for(Object object : components){
+            if(component.getClass().equals(object.getClass())){
+                return object;
+            }
+        }
+        return null;
+    }
+
+    public void addComponent(Object o){
+        components.add(o);
+>>>>>>> Stashed changes
     }
 
 }
