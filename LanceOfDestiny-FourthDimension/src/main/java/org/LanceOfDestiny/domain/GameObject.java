321fe79c0e7786b78109b20bfc772d416ce9ff28
TunaCimen <100401642+TunaCimen@users.noreply.github.com>
2024-04-11 13:36:48 +0300

    looper

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/GameObject.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/GameObject.java
@@ -1,16 +1,20 @@
 package org.LanceOfDestiny.domain;
+import javax.swing.*;
 import java.util.*;
 
 public abstract class GameObject extends Behaviour{
 
-    private static ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
+    public JPanel sprite(){
+        return new JPanel(); // Default Sprite Given to everything.
+    }
+    private static ArrayList<Behaviour> gameObjects = new ArrayList<>();
 
     public GameObject() {
         GameObject.gameObjects.add(this);
     }
 
 
-    public static ArrayList<GameObject> getGameObjects() {
+    public static ArrayList<Behaviour> getGameObjects() {
         return gameObjects;
     }
 
--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/player/FireBall.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/player/FireBall.java
@@ -1,6 +1,20 @@
 package org.LanceOfDestiny.domain.player;
 
 import org.LanceOfDestiny.domain.GameObject;
+import org.LanceOfDestiny.ui.BallSprite;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class FireBall extends GameObject {
+
+    BallSprite bs;
+
+    public FireBall(){
+        bs = new BallSprite(40,40,40, Color.red);
+    }
+    @Override
+    public JPanel sprite() {
+        return bs;
+    }
 }
