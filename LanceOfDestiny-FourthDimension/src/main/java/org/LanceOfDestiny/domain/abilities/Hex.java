32336d5674ddbf373a2ca14a042dfd78ba51556d
idilissever <idilissever@gmail.com>
2024-04-11 16:16:06 +0300

    implemented hex gameobject

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/abilities/Hex.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/abilities/Hex.java
@@ -1,2 +1,38 @@
-package org.LanceOfDestiny.domain.abilities;public class Hex {
+package org.LanceOfDestiny.domain.abilities;
+
+import org.LanceOfDestiny.domain.Constants;
+import org.LanceOfDestiny.domain.GameObject;
+
+import java.util.ArrayList;
+
+public class Hex extends GameObject {
+
+    protected int x;
+    protected int y;
+
+    public static final int HEX_WIDTH = Constants.HEX_LENGTH;
+    public static final int HEX_HEIGHT = Constants.HEX_LENGTH;
+    public static final double HEX_SPEED = Constants.HEX_SPEED;
+
+    public static ArrayList<Hex> hexes = new ArrayList<>();
+
+    public Hex(int x, int y) {
+        super();
+        this.x = x;
+        this.y = y;
+        addHex(this);
+
+
+    }
+
+    public static ArrayList<Hex> getHexes() {
+        return hexes;
+    }
+    public static void setHexes(ArrayList<Hex> hexes) {
+        Hex.hexes = hexes;
+    }
+
+    public void addHex(Hex hex) {
+        hexes.add(hex);
+    }
 }
