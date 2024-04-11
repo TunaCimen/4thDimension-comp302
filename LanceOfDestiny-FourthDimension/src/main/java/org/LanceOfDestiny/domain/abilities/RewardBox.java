65cb0c86dc61cba683b215b3a9b6a649331f5c13
idilissever <idilissever@gmail.com>
2024-04-11 16:35:28 +0300

    rewardBox implemented

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/abilities/RewardBox.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/abilities/RewardBox.java
@@ -3,5 +3,25 @@ package org.LanceOfDestiny.domain.abilities;
 import org.LanceOfDestiny.domain.GameObject;
 
 public class RewardBox extends GameObject {
+    public int x;
+    public int y;
 
+    private SpellType spellType;
+
+    private boolean isFalling;
+
+    public RewardBox(int x, int y, SpellType spellType) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.spellType = spellType;
+        this.isFalling = false;
+    }
+
+    public boolean isFalling() {
+        return isFalling;
+    }
+    public void setFalling(boolean isFalling) {
+        this.isFalling = isFalling;
+    }
 }
