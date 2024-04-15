a1dd753c36d3e9a58b1bfe8555fc254b0f48a2b5
TunaCimen <100401642+TunaCimen@users.noreply.github.com>
2024-04-15 15:43:42 +0300

    Pause resume modified

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/GameViews/GameView.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/GameViews/GameView.java
@@ -27,19 +27,21 @@ public class GameView extends JFrame implements Window {
         setResizable(false);
 
         //Buttons
-        JPanel buttonPanel = new JPanel();
         JPanel pausePanel =  new JPanel();
-        buttonPanel.setPreferredSize(new Dimension(Constants.SCREEN_WIDTH, 50));
+        pausePanel.setPreferredSize(new Dimension(Constants.SCREEN_WIDTH,50));
         JButton button = new JButton("Start Game");
         JButton buttonPause = new JButton("Pause Game");
         JButton buttonResume = new JButton("Resume Game");
-        buttonPanel.add(button);
+        buttonPause.setVisible(false);
+        buttonResume.setVisible(false);
+        pausePanel.add(button);
         pausePanel.add(buttonPause);
         pausePanel.add(buttonResume);
         addKeyListener(InputManager.getInstance());
         button.addActionListener(e->{
-            buttonPanel.setVisible(false);
-            sessionManager.getMagicalStaff().setPosition(Constants.STAFF_POSITION.add(new Vector(0,45)));
+            buttonPause.setVisible(true);
+            buttonResume.setVisible(true);
+            button.setVisible(false);
             sessionManager.loopExecutor.start();
         });
         buttonPause.addActionListener(e->{
@@ -50,7 +52,6 @@ public class GameView extends JFrame implements Window {
 
         //Add panels to frame.
         add(sessionManager.drawCanvas,BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
         add(pausePanel,BorderLayout.NORTH);
 
         //Initialize the Game Objects
