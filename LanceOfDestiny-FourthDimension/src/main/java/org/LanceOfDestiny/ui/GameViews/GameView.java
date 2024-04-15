b6529b7b9eec738142ea43be38b6654a205d7f91
TunaCimen <100401642+TunaCimen@users.noreply.github.com>
2024-04-15 15:37:58 +0300

    Pause resume

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/GameViews/GameView.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/GameViews/GameView.java
@@ -1,14 +1,13 @@
 package org.LanceOfDestiny.ui.GameViews;
 
 import org.LanceOfDestiny.domain.Constants;
+import org.LanceOfDestiny.domain.EventSystem.Events;
 import org.LanceOfDestiny.domain.managers.InputManager;
 import org.LanceOfDestiny.domain.managers.SessionManager;
 import org.LanceOfDestiny.domain.physics.Vector;
-import org.LanceOfDestiny.ui.DrawCanvas;
 import org.LanceOfDestiny.ui.Window;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class GameView extends JFrame implements Window {
@@ -29,20 +28,30 @@ public class GameView extends JFrame implements Window {
 
         //Buttons
         JPanel buttonPanel = new JPanel();
+        JPanel pausePanel =  new JPanel();
         buttonPanel.setPreferredSize(new Dimension(Constants.SCREEN_WIDTH, 50));
         JButton button = new JButton("Start Game");
+        JButton buttonPause = new JButton("Pause Game");
+        JButton buttonResume = new JButton("Resume Game");
         buttonPanel.add(button);
+        pausePanel.add(buttonPause);
+        pausePanel.add(buttonResume);
         addKeyListener(InputManager.getInstance());
         button.addActionListener(e->{
             buttonPanel.setVisible(false);
             sessionManager.getMagicalStaff().setPosition(Constants.STAFF_POSITION.add(new Vector(0,45)));
-
             sessionManager.loopExecutor.start();
+        });
+        buttonPause.addActionListener(e->{
+            Events.PauseGame.invoke();});
+        buttonResume.addActionListener(e->{
+            Events.ResumeGame.invoke();
         });
 
         //Add panels to frame.
         add(sessionManager.drawCanvas,BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
+        add(pausePanel,BorderLayout.NORTH);
 
         //Initialize the Game Objects
         sessionManager.initializeSession();
