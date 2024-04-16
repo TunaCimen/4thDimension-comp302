100ed0ddbe8103e7eb25ef793d14e9097ce60baf
TunaCimen <100401642+TunaCimen@users.noreply.github.com>
2024-04-16 19:58:20 +0300

    PauseWindow added

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/Windows.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/Windows.java
@@ -1,13 +1,15 @@
 package org.LanceOfDestiny.ui;
 
 import org.LanceOfDestiny.domain.managers.SessionManager;
+import org.LanceOfDestiny.ui.AuthViews.PauseView;
 import org.LanceOfDestiny.ui.AuthViews.PhysicsTestView;
 import org.LanceOfDestiny.ui.GameViews.GameView;
 
 public enum Windows{
         //Login(new LogInView(LogInController.getInstance())),
         PhysicsTest(new PhysicsTestView()),
-        GameView(new GameView(SessionManager.getInstance()));
+        GameView(new GameView(SessionManager.getInstance())),
+        PauseView(new PauseView());
 
         final Window window;
 
