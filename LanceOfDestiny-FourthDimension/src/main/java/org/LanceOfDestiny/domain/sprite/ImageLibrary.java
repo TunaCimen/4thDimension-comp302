134da49a3ebd1143828215d109252108fdc22576
TunaCimen <100401642+TunaCimen@users.noreply.github.com>
2024-04-16 20:18:54 +0300

    forgot sth not related to anything

---
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/sprite/ImageLibrary.java
@@ -0,0 +1,28 @@
+package org.LanceOfDestiny.domain.sprite;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public enum ImageLibrary {
+
+    FireBall("LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/Resources/Image/FireBallImage.png"),
+    RewardingBarrier("LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/Resources/Image/200Greengem 2.png"),
+    HealthBar("LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/Resources/Image/200Heart.png"),
+    MagicalStaff("LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/Resources/Image/200Player.png");
+
+    private final BufferedImage image;
+
+    ImageLibrary(String path){
+        try {
+            this.image = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("Path : %s doesnt exist", path));
+        }
+    }
+
+    public BufferedImage getImage(){
+        return image;
+    }
+}
