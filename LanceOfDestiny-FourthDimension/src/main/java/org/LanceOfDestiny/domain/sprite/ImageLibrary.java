3948dcc33b09c722c662e1a4238e894257ffaca1
idilissever <idilissever@gmail.com>
2024-04-21 12:55:16 +0300

    image paths changed

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/sprite/ImageLibrary.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/sprite/ImageLibrary.java
@@ -7,24 +7,24 @@ import java.io.IOException;
 
 public enum ImageLibrary {
 
-    FireBall("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/200Fireball.png"),
-    RewardingBarrier("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/200Greengem.png"),
-    Heart("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/200Heart.png"),
-    MagicalStaff("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/200Player.png"),
-    RewardBox("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/RewardBox.png"),
-    CannonSpell("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/CannonSpell.png"),
-    OverWhelmingSpell("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/Overwhelm.png"),
-    ExpansionSpell("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/expansion.png"),
-    ExplosiveBarrier("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/bomb.png"),
-    SimpleBarrier("/Users/feridun/Desktop/Workspace/Java/4thDimension-comp302/LanceOfDestiny-FourthDimension/Image/BlueGem.png");
+    FireBall("Image/200Fireball.png"),
+    RewardingBarrier("Image/200Greengem.png"),
+    Heart("Image/200Heart.png"),
+    MagicalStaff("Image/200Player.png"),
+    RewardBox("Image/RewardBox.png"),
+    CannonSpell("Image/CannonSpell.png"),
+    OverWhelmingSpell("Image/Overwhelm.png"),
+    ExpansionSpell("Image/expansion.png"),
+    ExplosiveBarrier("Image/bomb.png"),
+    SimpleBarrier("Image/BlueGem.png");
 
     private final BufferedImage image;
 
     ImageLibrary(String path){
         try {
             
-            File f = new File(path);
-            this.image = ImageIO.read(f);
+            var f = new File(path).getAbsolutePath();
+            this.image = ImageIO.read(new File(f));
             if(this.image == null)throw new RuntimeException("Image not found but loaded");
         } catch (IOException e) {
             throw new RuntimeException(String.format("Path : %s doesnt exist", path));
