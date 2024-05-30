b2814b9a382bbdab37b15c32c9f74cb23412f414
idilissever <idilissever@gmail.com>
2024-05-30 18:35:54 +0300

    spell duration init fixed

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/UIElements/SpellUIElement.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/ui/UIElements/SpellUIElement.java
@@ -14,15 +14,11 @@ public class SpellUIElement extends JLayeredPane {
     private JButton spellButton;
     private JProgressBar progressBar;
 
-    private Dimension preferredDimension;
-
-    private int duration;
-
     public SpellUIElement(ImageIcon imageIcon, Dimension size){
         this.imageIcon = imageIcon;
         reducedImageIcon = ImageOperations.reducedTransparencyImageIcon(imageIcon);
         this.setPreferredSize(size);
-        progressBar = new JProgressBar(0, duration);
+        progressBar = new JProgressBar(0, Constants.SPELL_DURATION);
         spellButton = new JButton();
         spellButton.setBorder(BorderFactory.createEmptyBorder());
         spellButton.setContentAreaFilled(false);
