5549105c5ac7f6a18009f4e9c1a963cc97a857d2
TunaCimen <100401642+TunaCimen@users.noreply.github.com>
2024-05-30 18:03:32 +0300

    fireball is a comet now. Maybe add it only on overwhelming.

--- LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/sprite/BallSprite.java
+++ LanceOfDestiny-FourthDimension/src/main/java/org/LanceOfDestiny/domain/sprite/BallSprite.java
@@ -1,25 +1,35 @@
 package org.LanceOfDestiny.domain.sprite;
-
 import org.LanceOfDestiny.domain.behaviours.GameObject;
+import org.LanceOfDestiny.domain.physics.Vector;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 public class BallSprite extends Sprite {
 
     static List<BallSprite> ballList = new ArrayList<>();
     private int radius;
+    Queue<Vector> lastPositions;
+    public boolean isAnimated;
 
     public BallSprite(GameObject attachedObject, Color color, int radius) {
         super(attachedObject, color);
+        isAnimated = false;
         ballList.add(this);
         this.radius = radius;
         this.color = color;
+        lastPositions = new ArrayDeque<>();
+
+
     }
 
     @Override
     public void drawShape(Graphics g) {
+        lastPositions.add(attachedGameObject.getPosition());
+        if(lastPositions.size() == 5)lastPositions.remove();
+
+
         g.fillOval((int) attachedGameObject.getPosition().getX() - radius,
                 (int) attachedGameObject.getPosition().getY() - radius,
                 2 * radius, 2 * radius);
@@ -28,6 +38,23 @@ public class BallSprite extends Sprite {
                     , ((int) attachedGameObject.getPosition().getX() - width())
                     , ((int) attachedGameObject.getPosition().getY() - height())
                     ,null);
+        }
+
+
+        int i = 0;
+        int tailSize = lastPositions.size();
+        for (Vector e : lastPositions) {
+            int currentRadius = (int) (radius * Math.pow(0.8, tailSize - i));
+            g.fillOval((int) e.getX() - currentRadius,
+                    (int) e.getY() - currentRadius,
+                    2 * currentRadius, 2 * currentRadius);
+            if (image != null) {
+                g.drawImage(getImage(),
+                        (int) e.getX() - currentRadius,
+                        (int) e.getY() - currentRadius,
+                        2 * currentRadius, 2 * currentRadius, null);
+            }
+            i++;
         }
 
     }
